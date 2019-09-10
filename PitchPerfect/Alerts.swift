095eaf16b48e7e48899948be691477653ef14a58
codebendr-product-designer <codebendr@gmail.com>
2019-09-10 04:03:54 +0000

    added alert struct

---
+++ PitchPerfect/Alerts.swift
@@ -0,0 +1,29 @@
+//
+//  Alerts.swift
+//  PitchPerfect
+//
+//  Created by codebendr on 09/09/2019.
+//  Copyright © 2019 just pixel. All rights reserved.
+//
+
+import UIKit
+
+struct Alerts {
+    static let DismissAlert = "Dismiss"
+    static let RecordingDisabledTitle = "Recording Disabled"
+    static let RecordingDisabledMessage = "You've disabled this app from recording your microphone. Check Settings."
+    static let RecordingFailedTitle = "Recording Failed"
+    static let RecordingFailedMessage = "Something went wrong with your recording."
+    static let AudioRecorderError = "Audio Recorder Error"
+    static let AudioSessionError = "Audio Session Error"
+    static let AudioRecordingError = "Audio Recording Error"
+    static let AudioFileError = "Audio File Error"
+    static let AudioEngineError = "Audio Engine Error"
+    
+    static func show(_ title: String, message: String) -> UIAlertController {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: Alerts.DismissAlert, style: .default, handler: nil))
+        return alert
+       
+    }
+}
